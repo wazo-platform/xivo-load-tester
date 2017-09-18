e9fbe18bcee68c2af3607bc47647c96069da3c68
Pascal Cadotte Michaud <pcm@wazo.community>
2017-09-18 11:08:40 -0400

    add a script to generate http requests
    This script connects to xivo-auth to get a token and then do an AMI action using xivo-amid.
    
    This is done at each second for 8 hours

---
+++ scripts/load-amid.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+USERNAME="${USERNAME:-test}"
+PASSWORD="${PASSWORD:-test}"
+BACKEND="${BACKEND:-xivo_service}"
+SLEEPTIME="${SLEEPTIME:-1}"
+RUNTIME="${RUNTIME:-$(echo "3600 * 8 / ${SLEEPTIME}" | bc)}"
+
+if ! pidof asterisk > /dev/null; then
+	echo 'Asterisk is not running'
+	exit 0
+fi
+
+for ((n=0;n<${RUNTIME};n++)); do
+	token=$(curl -k -i POST -H 'Content-Type: application/json' -u "${USERNAME}:${PASSWORD}" "https://localhost:9497/0.1/token" -d "{\"backend\": \"${BACKEND}\"}" 2>/dev/null | tail -n 1 | jq  -r ".data | .token")
+	curl -k -i -s -X POST -H 'Content-Type: application/json' -H 'Accept: application/json' -H "X-Auth-Token: ${token}" 'https://pcm-dev-0:9491/1.0/action/QueueSummary'
+	sleep 1
+done
+
+
